c0814c5748e88ae9c94fceb81180a665cd11e22f
andy <andyy0216@gmail.com>
2024-08-01 11:37:10 +0000

    build:route & controller setup

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/',(req, res) => {
-    res.send('<h1>express app for movies<h1>')
+    res.redirect('/movies')
+})
+
+app.get('/movies', (req, res) => {
+    res.send('<h1>listing movies<h1>')
+})
+
+app.get('/movie/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`read movie:${id}`)
 })
 
 app.listen(port, () => {
