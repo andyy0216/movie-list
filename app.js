38ae801fdedae7a4aad5a1f6d919422993b88224
andy <andyy0216@gmail.com>
2024-09-02 01:32:23 +0000

    remove wrong div

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const port = 3000
 const movies = require('./public/jsons/movies.json').results
 const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
 
-app.get('/movies', (req, res) => {
-    res.render('index', { movies, BASE_IMG_URL })
-})
-
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
@@ -19,15 +15,31 @@ app.get('/',(req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-    // res.send('listing movies')
-    res.render('index')
-})
+    const keyword = req.query.search
+    // if ( !keyword ) {
+    //     res.render('index', { movies, BASE_IMG_URL })
+    // } else {
+    //     const matchedMovies = movies.filter((mv) => mv.title.toLowerCase().includes(keyword.toLowerCase()))
+    //     res.render('index', { movies: matchedMovies, BASE_IMG_URL, keyword })
+    // }
+    // const matchedMovies = movies.filter((mv) => mv.title.toLowerCase().includes(keyword.toLowerCase()))
+
+    const matchedMovies = keyword ? movies.filter((mv) =>
+        Object.values(mv).some((property) => {
+          if (typeof property === 'string') {
+            return property.toLowerCase().includes(keyword.toLowerCase())
+          }
+          return false
+        })
+      ) : movies
+      res.render('index', { movies: matchedMovies, BASE_IMG_URL, keyword })
+    })
 
 app.get('/movie/:id', (req, res) => {
     const id = req.params.id
     const movie = movies.find((mv) => mv.id.toString() === id)
     res.render('detail', { movie, BASE_IMG_URL })
-})
+    })
 
 app.listen(port, () => {
     console.log(`express server in running on http://localhost:${port}`)
